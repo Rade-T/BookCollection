dcf932e9a64ad51314425022ea53f9ecc3f5c01a
Rade <Rade@RADE-PC>
2018-05-13 10:27:19 +0200

    Add project files.

---
+++ BookCollection/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookCollection.Startup))]
+namespace BookCollection
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
